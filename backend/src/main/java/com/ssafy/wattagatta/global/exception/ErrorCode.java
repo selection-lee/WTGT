0c86bab4bf409bf404b9bab95c07c97bb3f8b908
gnsals0904 <gnsals0904@naver.com>
2024-11-07 15:59:28 +0900

    [Feat] Constraint 클래스 구현

--- backend/src/main/java/com/ssafy/wattagatta/global/exception/ErrorCode.java
+++ backend/src/main/java/com/ssafy/wattagatta/global/exception/ErrorCode.java
@@ -12,8 +12,10 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, 1000, "해당 ID의 사용자를 찾을 수 없습니다."),
 
     // Product - 3000
-    INVALID_REGION_CODE(HttpStatus.BAD_REQUEST, 3000, "유효하지 않은 지역 코드입니다.");
+    INVALID_REGION_CODE(HttpStatus.BAD_REQUEST, 3000, "유효하지 않은 지역 코드입니다."),
 
+    // Agent - 4000
+    CANNOT_FIND_NEW_PATH(HttpStatus.NOT_FOUND, 4000, "에이전트의 경로를 찾지 못했습니다");
     private final HttpStatus status;
     private final int code;
     private final String message;
