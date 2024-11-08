f6ee8c138934d6300678f4c598de41de35ec6c59
이정하 <dlchris19@gmail.com>
2024-11-08 09:54:39 +0900

    [Fix] 토큰 만료 유효시간 24h로 변경

--- backend/src/main/java/com/ssafy/wattagatta/domain/auth/filter/LoginFilter.java
+++ backend/src/main/java/com/ssafy/wattagatta/domain/auth/filter/LoginFilter.java
@@ -55,7 +55,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         String role = auth.getAuthority();
 
-        String token = jwtUtil.createJwt(username, role, 60 * 60 * 10L);
+        String token = jwtUtil.createJwt(username, role, 60 * 60 * 24L);
 
         response.addHeader("Authorization", "Bearer " + token);
     }
