96bd992e65df9879499aabf86f0125967089a76f
gnsals0904 <gnsals0904@naver.com>
2024-11-13 09:31:30 +0900

    [Feat] 제약조건 추가

--- backend/src/main/java/com/ssafy/wattagatta/domain/agent/utils/PathStore.java
+++ backend/src/main/java/com/ssafy/wattagatta/domain/agent/utils/PathStore.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public class PathStore {
 
     private final Map<String, Map<Integer, Node>> agentPaths = Collections.synchronizedMap(new HashMap<>());
+    private final List<Constraint> globalConstraints = new ArrayList<>();
 
     public void setPosition(String agentId, int timeStep, Node position) {
         agentPaths.computeIfAbsent(agentId, k -> Collections.synchronizedMap(new HashMap<>())).put(timeStep, position);
